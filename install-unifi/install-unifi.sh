b8727600580c30270f3b13af7ea48c11c36d3599
Alex Wasserman <alex@alexwasserman.com>
2019-05-19 16:25:57 -0400

    Updating mongo catching and killing.

--- install-unifi/install-unifi.sh
+++ install-unifi/install-unifi.sh
@@ -35,26 +35,30 @@ FREEBSD_PACKAGE_LIST_URL="https://pkg.freebsd.org/${ABI}/latest/packagesite.txz"
 # Stop the controller if it's already running...
 # First let's try the rc script if it exists:
 if [ -f /usr/local/etc/rc.d/unifi ]; then
-	echo -n "Unifi already installed"
-  echo -n "Stopping the unifi service..."
+	echo "Unifi already installed"
+  echo "Stopping the unifi service..."
   /usr/sbin/service unifi stop
   echo " done."
 fi
 
 # Then to be doubly sure, let's make sure ace.jar isn't running for some other reason:
 if [ $(ps ax | grep -c "/usr/local/UniFi/lib/[a]ce.jar start") -ne 0 ]; then
-  echo -n "Killing ace.jar process..."
+  echo "Killing ace.jar process..."
   /bin/kill -15 `ps ax | grep "/usr/local/UniFi/lib/[a]ce.jar start" | awk '{ print $1 }'`
-  echo " done."
+  echo "Finishing shutdown down Unifi"
 fi
 
-# Try and stop mongo first using the service
-/usr/local/bin/mongod --dbpath '/usr/local/UniFi/data/db' --shutdown
+# Try and stop mongo first using mongo itself
+echo "Stopping Mongo"
+if [ $(ps ax | grep -c "/usr/local/UniFi/data/[d]b") -ne 0 ]; then
+  /usr/local/bin/mongo --port 27117 --eval "db.getSiblingDB('admin').shutdownServer()"
+fi
 
 # Check if mongo is still running by making sure mongodb doesn't have the db file open
 # killing it after if it's running:
 if [ $(ps ax | grep -c "/usr/local/UniFi/data/[d]b") -ne 0 ]; then
-  echo -n "Killing mongod process..."
+	echo "Mongo is still running"
+  echo "Killing mongod process..."
   /bin/kill -15 `ps ax | grep "/usr/local/UniFi/data/[d]b" | awk '{ print $1 }'`
   echo " done."
 fi
@@ -68,19 +72,19 @@ fi
 
 # Add the fstab entries apparently required for OpenJDKse:
 if [ $(grep -c fdesc /etc/fstab) -eq 0 ]; then
-  echo -n "Adding fdesc filesystem to /etc/fstab..."
+  echo "Adding fdesc filesystem to /etc/fstab..."
   echo -e "fdesc\t\t\t/dev/fd\t\tfdescfs\trw\t\t0\t0" >> /etc/fstab
   echo " done."
 fi
 
 if [ $(grep -c proc /etc/fstab) -eq 0 ]; then
-  echo -n "Adding procfs filesystem to /etc/fstab..."
+  echo "Adding procfs filesystem to /etc/fstab..."
   echo -e "proc\t\t\t/proc\t\tprocfs\trw\t\t0\t0" >> /etc/fstab
   echo " done."
 fi
 
 # Run mount to mount the two new filesystems:
-echo -n "Mounting new filesystems..."
+echo "Mounting new filesystems..."
 /sbin/mount -a
 echo " done."
 
@@ -205,7 +209,7 @@ chmod +x /usr/local/etc/rc.d/unifi
 # Eventually, this step will need to be folded into pfSense, which manages the main rc.conf.
 # In the following comparison, we expect the 'or' operator to short-circuit, to make sure the file exists and avoid grep throwing an error.
 if [ ! -f /etc/rc.conf.local ] || [ $(grep -c unifi_enable /etc/rc.conf.local) -eq 0 ]; then
-  echo -n "Enabling the unifi service..."
+  echo "Enabling the unifi service..."
   echo "unifi_enable=YES" >> /etc/rc.conf.local
   echo " done."
 fi
@@ -218,6 +222,6 @@ if [ ! -z "${BACKUPFILE}" ] && [ -f ${BACKUPFILE} ]; then
 fi
 
 # Start it up:
-echo -n "Starting the unifi service..."
+echo "Starting the unifi service..."
 /usr/sbin/service unifi start
 echo " done."
